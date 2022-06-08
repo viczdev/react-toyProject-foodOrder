3512f8af92e264fddcc4da15f8da44086dbfa386
Victor Zhang <victorzhangdev@gmail.com>
2022-06-08 16:49:06 -0700

    finish the basic ui and cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onCloseCart}>
+      {cartItems}
+      <div>
+        <span>Total Amount</span>
+        <span>1000</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
